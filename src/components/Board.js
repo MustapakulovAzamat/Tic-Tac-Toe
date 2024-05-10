64c05b90800b5c0f0fb16f422f65747b5de6e9fc
Mustapakulov Azamat <azamustapakylov6@gmail.com>
2024-05-11 00:07:16 +0600

    add count win

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,68 +1,91 @@
-import React, { useState } from 'react'
-import { Square } from './Square'
+import React, { useState, useEffect, useCallback } from 'react';
+import { Square } from './Square';
+
 export const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null))
-    const [isNextX, setIsNextX] = useState(true)
+    const [squares, setSquares] = useState(Array(9).fill(null));
 
-    let info = `Next - ${(isNextX) ? 'x' : 'x'}`
-    let winnerInfo = null
+    const [isNextX, setIsNextX] = useState(true);
 
-    let isWinner = false
+    const [winners, setWinners] = useState({ x: 0, o: 0 });
 
-    const winnerFunc = () => {
+    let info = `Next - ${(isNextX) ? 'x' : 'o'}`;
+    let winnerInfo = null;
+
+    const winnerFunc = useCallback(() => {
         const winnerCombinations = [
-            [0,1,2], //по горизонтали
-            [3,4,5],
-            [6,7,8],
+            [0, 1, 2], // по горизонтали
+            [3, 4, 5],
+            [6, 7, 8],
 
-            [0,3,6], //по вертикали
-            [1,4,7],
-            [2,5,8],
+            [0, 3, 6], // по вертикали
+            [1, 4, 7],
+            [2, 5, 8],
 
-            [0,4,8], //по диоганали
-            [6,4,2]
-        ]
-        for (let combinantion of winnerCombinations) {
-            let [a, b, c] = combinantion
-            if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-                isWinner = true
-                return squares[a]
+            [0, 4, 8], // по диагонали
+            [6, 4, 2]
+        ];
+
+        for (let combination of winnerCombinations) {
+            let [a, b, c] = combination;
+            if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+                return squares[c];
             }
         }
-        return null
-    }
-
-
-    winnerInfo = winnerFunc()
-    if(winnerInfo) info = `Winner - ${winnerInfo}`
-
-    //следущий ход
-    const setSquaresValue = (index) => {
-
-        if(isWinner) {
+        const fullBoard = squares.every(squares => squares)
+        if (fullBoard) {
             window.location.reload()
             return
         }
 
-        if (squares[index]) return
+        return null;
+    }, [squares])
 
 
-        let newSquares = squares.slice()
-        newSquares[index] = isNextX ? 'x' : 'o'
-        setIsNextX(!isNextX)
-        setSquares(newSquares)
-    }
-  return (
-    <div>
-        <h1>{info}</h1>
-       {squares.map((square, index) => {
-        return (
-            <span key={index} className='span'>
-            <Square value={square} setSquaresValue={() => {setSquaresValue(index)}}/>
-            {(index === 2 || index === 5) && <br />}
-            </span>
-        )
-       })}
-    </div>
-  )
-}
+
+    useEffect(() => {
+        const winner = winnerFunc();
+        if (winner) {
+            setWinners(prevWinners => {
+                return {
+                    ...prevWinners,
+                    [winner]: prevWinners[winner] + 1
+                };
+            });
+        }
+    }, [squares, winnerFunc]);
+
+    winnerInfo = winnerFunc();
+    if (winnerInfo) info = `Winner - ${winnerInfo}`;
+
+    // следующий ход
+    const setSquaresValue = (index) => {
+        if (winnerInfo) {
+            window.location.reload()
+            return
+        } 
+        if(squares[index]) return
+
+        let newSquares = squares.slice();
+        newSquares[index] = isNextX ? 'x' : 'o';
+        setIsNextX(!isNextX);
+        setSquares(newSquares);
+    };
+
+    return (
+        <div>
+            <h1>{info}</h1>
+            <div>
+                <h2>X win - {winners.x}</h2>
+                <h2>O win - {winners.o}</h2>
+            </div>
+            {squares.map((square, index) => {
+                return (
+                    <span key={index} className='span'>
+                        <Square value={square} setSquaresValue={() => { setSquaresValue(index) }} />
+                        {(index === 2 || index === 5) && <br />}
+                    </span>
+                );
+            })}
+        </div>
+    );
+};
